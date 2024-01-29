95e046426c137bd01e293cb8c78bb7b4f786457b
Versina Vasile <ipvvg4@gmail.com>
2024-01-29 18:34:49 +0100

    add lesson_54

--- src/Consultation/Practice_07/Text/ExampleTest.java
+++ src/Consultation/Practice_07/Text/ExampleTest.java
@@ -1,2 +1,27 @@
-package Consultation.Practice_07.Text;public class ExempleTest {
+package Consultation.Practice_07.Text;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+  // org.junit.jupiter:junit-jupiter:5.9.1
+
+  @Test
+  void stringBuilderAppendsProperly() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello");
+    assertEquals("Hello", sb.toString());
+  }
+
+  @Test
+  void stringBuilderNonEmptyAfterAppend() {
+    StringBuilder sb = new StringBuilder();
+    assertTrue(sb.toString().isEmpty());
+    sb.append("a");
+    assertFalse(sb.toString().isEmpty());
+  }
 }
---
+++ src/lesson_54/exemple06/Main.java
@@ -0,0 +1,2 @@
+package lesson_54.exemple06;public class Main {
+}
